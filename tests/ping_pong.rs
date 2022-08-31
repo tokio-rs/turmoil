6a7bd4fd22ff42c2ce9476edda95a88be334b0d3
Brett Mcchesney <mcches@amazon.com>
2022-08-31 12:19:06 -0600

    Fix termination bug in Sim::run()
    The flag for `is_finished` needs to ensure *all* clients are finished
    rather than just the last one.

--- tests/ping_pong.rs
+++ tests/ping_pong.rs
@@ -1,6 +1,6 @@
-use std::{matches, time::Duration};
-use tokio::time::timeout;
-use turmoil::Builder;
+use std::{matches, rc::Rc, time::Duration};
+use tokio::{sync::Semaphore, time::timeout};
+use turmoil::{Builder, Io};
 
 #[derive(Debug)]
 enum Message {
@@ -61,4 +61,34 @@ fn network_partition() {
     });
 
     sim.run();
+}
+
+#[test]
+fn multiple_clients_all_finish() {
+    let how_many = 3;
+    let tick_ms = 10;
+
+    // N = how_many runs, each with a different client finishing immediately
+    for run in 0..how_many {
+        let mut sim = Builder::new()
+            .tick_duration(Duration::from_millis(tick_ms))
+            .build();
+
+        let ct = Rc::new(Semaphore::new(how_many));
+
+        for client in 0..how_many {
+            let ct = ct.clone();
+
+            sim.client(format!("client-{}", client), |_: Io<Message>| async move {
+                let ms = if run == client { 0 } else { 2 * tick_ms };
+                tokio::time::sleep(Duration::from_millis(ms)).await;
+
+                let p = ct.acquire().await.unwrap();
+                p.forget();
+            });
+        }
+
+        sim.run();
+        assert_eq!(0, ct.available_permits());
+    }
 }
