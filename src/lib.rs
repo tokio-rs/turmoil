aaaf069961eb2c1223fc2f0a35db6b7ecaf1ca9d
mcches <39924297+mcches@users.noreply.github.com>
2022-09-06 15:25:38 -0600

    Add distinct connections over Io (#18)
    Real world servers often use concurrency for processing requests.
    Protocols also lean on message ordering per connection, ie TCP. Neither
    of these features were available with the current `Io` implementation.
    
    This change overlays connection support on top of `Io` by multiplexing
    through an actor. A simple TCP-like state machine is used to initiate and
    accept connections.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ pub use builder::Builder;
 mod config;
 use config::Config;
 
+mod connection;
+pub use connection::{Connection, ConnectionIo, Segment};
+
 mod dns;
 use dns::Dns;
 pub use dns::ToSocketAddr;
