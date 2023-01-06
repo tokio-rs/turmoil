ecf271c1de6e6940b17041b352b9ffec7669cbda
Lucio Franco <luciofranco14@gmail.com>
2023-01-06 10:42:16 -0500

    Add more type conversions for ToSocketAddrs (#71)
    * Add more type conversions for ToSocketAddrs
    
    * Add dns parsing and test

--- src/dns.rs
+++ src/dns.rs
@@ -10,7 +10,9 @@ pub trait ToIpAddr {
     fn to_ip_addr(&self, dns: &mut Dns) -> IpAddr;
 }
 
-pub trait ToSocketAddrs {
+/// A simulated version of `tokio::net::ToSocketAddrs`.
+pub trait ToSocketAddrs: sealed::Sealed {
+    #[doc(hidden)]
     fn to_socket_addr(&self, dns: &Dns) -> SocketAddr;
 }
 
@@ -86,5 +88,65 @@ impl ToSocketAddrs for SocketAddr {
 impl ToSocketAddrs for (IpAddr, u16) {
     fn to_socket_addr(&self, _: &Dns) -> SocketAddr {
         (*self).into()
+    }
+}
+
+impl<T: ToSocketAddrs + ?Sized> ToSocketAddrs for &T {
+    fn to_socket_addr(&self, dns: &Dns) -> SocketAddr {
+        (**self).to_socket_addr(dns)
+    }
+}
+
+impl ToSocketAddrs for str {
+    fn to_socket_addr(&self, dns: &Dns) -> SocketAddr {
+        let socketaddr: Result<SocketAddr, _> = self.parse();
+
+        if let Ok(s) = socketaddr {
+            return s;
+        }
+
+        // Borrowed from std
+        // https://github.com/rust-lang/rust/blob/1b225414f325593f974c6b41e671a0a0dc5d7d5e/library/std/src/sys_common/net.rs#L175
+        macro_rules! try_opt {
+            ($e:expr, $msg:expr) => {
+                match $e {
+                    Some(r) => r,
+                    None => panic!("Unable to parse dns: {}", $msg),
+                }
+            };
+        }
+
+        // split the string by ':' and convert the second part to u16
+        let (host, port_str) = try_opt!(self.rsplit_once(':'), "invalid socket address");
+        let port: u16 = try_opt!(port_str.parse().ok(), "invalid port value");
+
+        (host, port).to_socket_addr(dns)
+    }
+}
+
+impl ToSocketAddrs for String {
+    fn to_socket_addr(&self, dns: &Dns) -> SocketAddr {
+        self.as_str().to_socket_addr(dns)
+    }
+}
+
+mod sealed {
+
+    pub trait Sealed {}
+
+    impl<T: ?Sized> Sealed for T {}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{dns::Dns, ToSocketAddrs};
+
+    #[test]
+    fn parse_str() {
+        let mut dns = Dns::new();
+        dns.names.insert("foo".into(), "127.0.0.1".parse().unwrap());
+        let s = "foo:5000".to_socket_addr(&dns);
+
+        assert_eq!(s, "127.0.0.1:5000".parse().unwrap());
     }
 }
