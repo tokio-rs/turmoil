5179c9844ad053bc07b8fb6a1e721b6f5f7edb5e
Brett Mcchesney <mcches@amazon.com>
2023-01-09 16:53:22 -0700

    Make tokio unstable opt in
    We use `unhandled_panic` to forward host software panics as test
    failures.
    
    Forcing this is a bit invasive for consumers of the crate, so
    instead we make it enabled conditionally.

--- src/rt.rs
+++ src/rt.rs
@@ -83,12 +83,13 @@ impl Rt {
 }
 
 fn init() -> (Runtime, LocalSet) {
-    let tokio = tokio::runtime::Builder::new_current_thread()
-        .enable_time()
-        .start_paused(true)
-        .unhandled_panic(tokio::runtime::UnhandledPanic::ShutdownRuntime)
-        .build()
-        .unwrap();
+    let mut builder = tokio::runtime::Builder::new_current_thread();
+
+    if cfg!(tokio_unstable) {
+        builder.unhandled_panic(tokio::runtime::UnhandledPanic::ShutdownRuntime);
+    }
+
+    let tokio = builder.enable_time().start_paused(true).build().unwrap();
 
     tokio.block_on(async {
         // Sleep to "round" `Instant::now()` to the closest `ms`
@@ -100,6 +101,10 @@ fn init() -> (Runtime, LocalSet) {
 
 fn new_local() -> LocalSet {
     let mut local = LocalSet::new();
-    local.unhandled_panic(tokio::runtime::UnhandledPanic::ShutdownRuntime);
+
+    if cfg!(tokio_unstable) {
+        local.unhandled_panic(tokio::runtime::UnhandledPanic::ShutdownRuntime);
+    }
+
     local
 }
