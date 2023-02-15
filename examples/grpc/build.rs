7c7d35bf60cbf3121d15242a766563d583ff7146
Lucio Franco <luciofranco14@gmail.com>
2023-02-15 10:13:25 -0500

    example(grpc): Remove protoc requirement (#86)

--- examples/grpc/build.rs
+++ examples/grpc/build.rs
@@ -1,3 +1,18 @@
+use prost::Message;
+use std::path::PathBuf;
+
 fn main() {
-    tonic_build::compile_protos("helloworld.proto").unwrap();
+    let file_descriptors = protox::compile(["helloworld.proto"], ["."]).unwrap();
+    let file_descriptor_path = PathBuf::from(std::env::var_os("OUT_DIR").expect("OUT_DIR not set"))
+        .join("file_descriptor_set.bin");
+    std::fs::write(&file_descriptor_path, file_descriptors.encode_to_vec()).unwrap();
+
+    let mut config = prost_build::Config::new();
+    config
+        .file_descriptor_set_path(&file_descriptor_path)
+        .skip_protoc_run();
+
+    tonic_build::configure()
+        .compile_with_config(config, &["helloworld.proto"], &["."])
+        .unwrap();
 }
