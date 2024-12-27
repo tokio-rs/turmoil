9cbdfa77404502e7801338acd670896535f3f83a
tottoto <tottotodev@gmail.com>
2024-12-28 08:07:12 +0900

    Use EnvFilter config to set default tracing level (#196)

--- examples/grpc/src/main.rs
+++ examples/grpc/src/main.rs
@@ -66,13 +66,13 @@ fn main() {
 /// An example of how to configure a tracing subscriber that will log logical
 /// elapsed time since the simulation started using `turmoil::sim_elapsed()`.
 fn configure_tracing() {
-    if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", "info");
-    }
-
     tracing::subscriber::set_global_default(
         tracing_subscriber::fmt()
-            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .with_env_filter(
+                tracing_subscriber::EnvFilter::builder()
+                    .with_default_directive(tracing::level_filters::LevelFilter::INFO.into())
+                    .from_env_lossy(),
+            )
             .with_timer(SimElapsedTime)
             .finish(),
     )
